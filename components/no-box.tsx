0be75d879f61795b760ae25873557cb60dacfcbe
KingTingTheGreat <jting@bu.edu>
2024-02-06 18:36:44 -0500

    styling

--- components/no-box.tsx
+++ components/no-box.tsx
@@ -1,6 +1,6 @@
 const NoBox = ({ incNo }: { incNo: any }) => {
 	return (
-		<div className="bg-red-300" onClick={incNo}>
+		<div className="bg-red-300 cursor-pointer" onClick={incNo}>
 			<h3>No</h3>
 		</div>
 	);
