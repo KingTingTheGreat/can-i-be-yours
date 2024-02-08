fbd1a4796e8855cadbf3366cf116c13985565eef
KingTingTheGreat <jting@bu.edu>
2024-02-08 17:39:01 -0500

    cors testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/api/hello", // Adjust the path to match your API routes
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://canibeyours.com", // Set to your app's domain
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type",
+					},
+				],
+			},
+		];
+	},
+};
 
 export default nextConfig;
