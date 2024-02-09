b8d2faf9f84b7e183674c82a47046e5bd5349bd5
KingTingTheGreat <jting@bu.edu>
2024-02-09 12:06:54 -0500

    correctly checks that we got something

--- app/api/getEntry/route.ts
+++ app/api/getEntry/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
 	const key = request.nextUrl.searchParams.get("key") as string;
 	const db = await entryDBConnect();
 	const entry = await db.Entry.find({ key }).exec();
-	if (!entry) {
+	if (entry.length === 0) {
 		return NextResponse.json({ error: "Entry not found" }, { status: 404 });
 	} else if (entry.length > 1) {
 		return NextResponse.json({ error: "Multiple entries found" }, { status: 500 });
