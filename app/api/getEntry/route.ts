25d0f65bee57e9245bb586eca1a7cf19c1afaf71
KingTingTheGreat <jting@bu.edu>
2024-02-09 11:06:01 -0500

    returns only one entry. erros if multiple are found

--- app/api/getEntry/route.ts
+++ app/api/getEntry/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import getEntry from "@/utils/getEntry";
 import { entryDBConnect } from "@/utils/connection";
 
 export const dynamic = "force-dynamic"; // defaults to auto
@@ -8,5 +7,10 @@ export async function GET(request: NextRequest) {
 	const key = request.nextUrl.searchParams.get("key") as string;
 	const db = await entryDBConnect();
 	const entry = await db.Entry.find({ key }).exec();
-	return NextResponse.json(entry);
+	if (!entry) {
+		return NextResponse.json({ error: "Entry not found" }, { status: 404 });
+	} else if (entry.length > 1) {
+		return NextResponse.json({ error: "Multiple entries found" }, { status: 500 });
+	}
+	return NextResponse.json(entry[0]);
 }
