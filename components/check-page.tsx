ca09d9f21232b35fefb4b897a216a6acd43ec4b8
KingTingTheGreat <jting@bu.edu>
2024-02-11 14:42:15 -0500

    uses the correct component

--- components/check-page.tsx
+++ components/check-page.tsx
@@ -1,20 +1,20 @@
 "use client";
 import React from "react";
 import { useSearchParams } from "next/navigation";
-import QuestionPage from "@/components/question-page";
+import CheckQuestion from "./check-question";
 import { redirect } from "next/navigation";
 
 const CheckPage = async () => {
 	const key = useSearchParams().get("key") as string;
 
-	const res = await fetch(`https://canibeyours.com/api/getEntry?key=${key}`);
+	const res = await fetch(`https://canibeyours.com/api/getEntry?key=${key}&checker=true`);
 	if (!res.ok) {
 		console.log("Failed to fetch");
 		redirect("/");
 	}
 	const data = await res.json();
 
-	return <QuestionPage data={data} />;
+	return <CheckQuestion data={data} />;
 };
 
 export default CheckPage;
