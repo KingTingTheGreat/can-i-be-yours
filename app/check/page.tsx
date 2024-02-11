064697d585bf2e95adcedab1aac74f3cef3ad058
KingTingTheGreat <jting@bu.edu>
2024-02-11 10:51:10 -0500

    removed commented out code

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -1,20 +1,7 @@
-// "use client";
 import React, { Suspense } from "react";
-// import { redirect } from "next/navigation";
-// import { useSearchParams } from "next/navigation";
 import CheckPage from "@/components/check-page";
 
 const SenderPage = () => {
-	// const searchParams = useSearchParams();
-	// const key = searchParams.get("key") as string;
-
-	// const res = await fetch(`https://canibeyours.com/api/getEntry?key=${key}`);
-	// if (!res.ok) {
-	// 	console.log("Failed to fetch");
-	// 	redirect("/");
-	// }
-	// const data = await res.json();
-
 	return (
 		<main>
 			<Suspense>
