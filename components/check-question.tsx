0de2f182f5218f59819dce6b210107199b9c31d3
KingTingTheGreat <jting@bu.edu>
2024-02-11 11:56:28 -0500

    page for users to check the responses to their messages

--- components/check-question.tsx
+++ components/check-question.tsx
@@ -1,38 +1,71 @@
 "use client";
-import AnswerBox from "./answer-box";
 import Loading from "./loading";
-import Question from "./question";
 import { useState, useEffect } from "react";
 
-const CheckQuestion = ({ data }: { data: { title: string; y: boolean; name: string } }) => {
-	const [numNo, setNumNo] = useState(0);
+const ResponseWrapper = ({ children }: { children: React.ReactNode }) => {
+	return <h2 className="text-2xl">{children}</h2>;
+};
+
+const CheckQuestion = ({
+	data,
+}: {
+	data: { title: string; y: boolean; name: string; opened: boolean; answer: boolean | null };
+}) => {
 	const [y, setY] = useState(true);
 	const [title, setTitle] = useState("");
 	const [name, setName] = useState("");
+	const [opened, setOpened] = useState(false);
+	const [answer, setAnswer] = useState<null | boolean>(null);
 
 	useEffect(() => {
 		setTitle(data.title);
 		setY(data.y);
 		setName(data.name);
+		setOpened(data.opened);
+		setAnswer(data.answer);
 	}, [data]);
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			{title ? (
-				<>
-					<Question y={y} title={title} name={name} />
-					<div className="flex">
-						<AnswerBox answer="yes" numNo={numNo} incNo={null} />
-						<AnswerBox answer="no" numNo={numNo} incNo={() => setNumNo(numNo + 1)} />
-					</div>
+	const getName = () => {
+		return <span className="font-medium">{name !== "" ? name : "Your recipient"}</span>;
+	};
 
-					<p className="select-none"># of times you said no: {numNo}</p>
-				</>
-			) : (
-				<Loading />
-			)}
-		</div>
-	);
+	const Response = () => {
+		if (!opened) {
+			return <ResponseWrapper>{getName()} has not opened your message yet.</ResponseWrapper>;
+		} else if (answer === null) {
+			return <ResponseWrapper>{getName()} has opened your message and is thinking it over.</ResponseWrapper>;
+		} else if (y) {
+			if (answer) {
+				return (
+					<ResponseWrapper>
+						{getName()} would like you to be their {title}!
+					</ResponseWrapper>
+				);
+			} else {
+				return (
+					<ResponseWrapper>
+						{getName()} does not want you to be their {title}... 😢
+					</ResponseWrapper>
+				);
+			}
+		} else {
+			if (answer) {
+				return (
+					<ResponseWrapper>
+						{getName()} would like to be your {title}!
+					</ResponseWrapper>
+				);
+			} else {
+				return (
+					<ResponseWrapper>
+						{getName()} does not want to be your {title}... 😢
+					</ResponseWrapper>
+				);
+			}
+		}
+	};
+
+	return <div className="flex flex-col justify-center items-center">{title ? <Response /> : <Loading />}</div>;
 };
 
 export default CheckQuestion;
